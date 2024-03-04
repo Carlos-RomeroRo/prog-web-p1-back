c0386f99d2058c4f4355fc2f5cdd761b86a31e86
Keiner Alvarado <kjgameplay0000@gmail.com>
2024-03-04 12:34:41 -0500

    fix: test example fix

--- src/test/java/desarrolloweb/progwebp1back/ProgWebP1BackApplicationTests.java
+++ src/test/java/desarrolloweb/progwebp1back/ProgWebP1BackApplicationTests.java
@@ -3,15 +3,16 @@ package desarrolloweb.progwebp1back;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+// import org.springframework.boot.test.context.SpringBootTest;
 
 // @SpringBootTest
 class ProgWebP1BackApplicationTests {
 
 	@Test
 	void contextLoads() {
-		//test example
-		assertEquals(1, 1);
+		// test example
+		int test = 1;
+		assertEquals(1, test);
 	}
 
 }
